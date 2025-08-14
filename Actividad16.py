588e0d1188f9de964a05e11d4c761a751cee07e3
Daniel88V <daniel12velo@gmail.com>
2025-08-13 19:24:37 -0600

    Actividad 16

--- Actividad16.py
+++ Actividad16.py
@@ -5,4 +5,15 @@ class Libro:
         self._autor = autor
         self._fecha_publicacion = fecha_publicacion
     def __str__(self):
-        return f"Libro: {self._titulo}"
+        return f"Libro: {self._titulo} | Autor: {self._autor} | Fecha: {self._fecha_publicacion}"
+class Usuario:
+    def __init__(self, carnet, nombre, carrera):
+        self._carnet = carnet
+        self._nombre = nombre
+        self._carrera = carrera
+    def __str__(self):
+        return f"Nombre: {self._nombre} | Carrera: {self._carrera}"
+class BibliotecaUsuarios:
+    def __init__(self):
+        self._usuarios = []
+
